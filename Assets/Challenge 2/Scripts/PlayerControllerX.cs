25867bdcbd12260ed46589160ccdf87c94ea2e53
Andreas Egger <andreasegger4@gmail.com>
2022-10-10 20:06:55 +0100

    finished callenege 2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float waitTime = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && waitTime <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            waitTime = 1;
+        }
+
+        if (waitTime > 0)
+        {
+            waitTime -= Time.deltaTime;
+        }
+
+    }
+}
